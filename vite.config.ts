526d32d2b08363faf39d9a954b45611a12a8ee5c
Aleksey Verin <verevaa@yandex.ru>
2023-08-02 12:14:57 +0300

    feat: changed the minimum width of the content

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/tables/',
+  server: {
+    host: true
+  },
   test: {
     globals: true,
     environment: 'jsdom'
